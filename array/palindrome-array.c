9472845d73d3ebb477339e2809aece10ac39e8bb
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-12 17:59:01 +0600

    Solved: palindrome array

--- array/palindrome-array.c
+++ array/palindrome-array.c
@@ -17,32 +17,71 @@ int main()
     int i = 0;
     int j = n - 1;
 
-    int flag = 0;
+    int flag = 1;
 
     while (i <= j)
     {
-        if (a[i] == a[j])
-        {
-            flag = 1;
-        }
-        else
+        if (a[i] != a[j])
         {
             flag = 0;
+            printf("NO");
             break;
         }
-
         i++;
         j--;
     }
 
     if (flag)
-    {
         printf("YES");
-    }
-    else
-    {
-        printf("NO");
-    }
 
     return 0;
 }
+
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     int n;
+
+//     scanf("%d", &n);
+
+//     int a[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a[i]);
+//     }
+
+//     int i = 0;
+//     int j = n - 1;
+
+//     int flag = 0;
+
+//     while (i <= j)
+//     {
+//         if (a[i] == a[j])
+//         {
+//             flag = 1;
+//         }
+//         else
+//         {
+//             flag = 0;
+//             break;
+//         }
+
+//         i++;
+//         j--;
+//     }
+
+//     if (flag)
+//     {
+//         printf("YES");
+//     }
+//     else
+//     {
+//         printf("NO");
+//     }
+
+//     return 0;
+// }
