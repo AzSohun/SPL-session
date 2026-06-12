a16d9030a081fddce4ed7817da8ef7128139e711
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-12 19:17:44 +0600

    Solved: Smallest Pair

---
+++ array/smallest-pair.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        scanf("%d", &n);
+
+        int a[n];
+
+        for (int i = 1; i <= n; i++)
+        {
+            scanf("%d", &a[i]);
+        }
+
+        int _i = 1;
+        int _j = 2;
+        int smallest_pair = a[_i] + a[_j] + _j - _i;
+
+        for (int i = _i; i <= n - 1; i++)
+        {
+            for (int j = _j; j <= n; j++)
+            {
+                int sum = a[i] + a[j] + j - i;
+
+                if (smallest_pair >= sum)
+                {
+                    smallest_pair = sum;
+                }
+
+                // printf("%d + %d + %d - %d = %d\n", a[i], a[j], j, i, sum);
+            }
+            _j++;
+        }
+
+        printf("%d\n", smallest_pair);
+    }
+}
